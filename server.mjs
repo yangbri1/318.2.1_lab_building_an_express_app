f10cd64d88be9707cbaba86b06f93ae2016c074c
brian <yangbri1@msu.edu>
2024-10-09 20:43:04 -0400

    included additional routes

--- server.mjs
+++ server.mjs
@@ -14,10 +14,21 @@ import express from 'express';
 const app = express();
 const PORT = 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello from the other side?');
-});
+// route methods 
 
+// when user makes GET request to root/homepage
+app.get('/', (req, res) => {
+    // shows this message
+    res.send('Hello from the other side...');
+});
+// GET request to /about page ...
+app.get('/about', (req, res) => {
+    res.send(`Don't forget about us...`);
+});
+// GET request to any other page ...
+app.get('*', (req, res) => {
+    res.send(`404 Error: Nothing to see here`);
+});
 
 /* app.listen() should always be the very last thing in the server - 
 although dealing w/ asynchronous JS but sequencing still mattter */
@@ -25,6 +36,8 @@ app.listen(PORT, () => {
     // console.log() w/ some string interpolation 
     console.log(`Server is runnning on PORT: ${PORT}`);
 });
+
 /* after importing Express, initializing Express to be contained in a variable "app", and
 incorporating app.listen() at the very end, we had essentially established an Express server */
+
 // "npm start" in CLI & "localhost.com/{PORT}" in browser to check"
